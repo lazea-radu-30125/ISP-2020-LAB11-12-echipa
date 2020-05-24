67e669ffcf555060f095ab3e65527cb895a4b020
lazea-radu-30125 <raducu_lazea@yahoo.com>
2020-05-25 02:03:20 +0300

    Exercise2 Online Shop

--- c4-samples/src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
@@ -3,11 +3,14 @@ package aut.utcluj.isp.ex2;
 /**
  * @author stefan
  */
-public class OnlineShop {
+public class OnlineShop extends Shop{
     private String webAddress;
+    private Shop shop;
 
     public OnlineShop(String name, String city, String webAddress) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        super(name,city);
+        this.shop = new Shop(name,city);
+        this.webAddress = webAddress;
     }
 
     public String getWebAddress() {
